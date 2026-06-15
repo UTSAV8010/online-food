5d71e7526bea6e30f8b6dc17a46dfc614f48c0f8
Utsav Sarvaliya <utsavsarvaliya27@gmail.com>
2026-06-15 17:56:47 +0530

    Update mail.php

--- frontend/config/mail.php
+++ frontend/config/mail.php
@@ -8,10 +8,11 @@ declare(strict_types=1);
 |--------------------------------------------------------------------------
 | Required for PHPMailer
 */
-require_once __DIR__ . '/../../vendor/autoload.php';
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+require_once __DIR__ . '/../PHPMailer/src/PHPMailer.php';
+require_once __DIR__ . '/../PHPMailer/src/SMTP.php';
+require_once __DIR__ . '/../PHPMailer/src/Exception.php';
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\Exception;
 
 /*
 |--------------------------------------------------------------------------
